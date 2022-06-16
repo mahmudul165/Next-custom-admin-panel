5682d2721cd9c0e574a823c722663c5de33d5b20
lamptechs <career@lamptechs.com>
2022-06-16 19:41:46 +0600

    added

--- components/CreatePatient/PatientForm.js
+++ components/CreatePatient/PatientForm.js
@@ -136,16 +136,13 @@ function PaitentForm({ title, data }) {
                         id="firstname"
                         {...register("firstname")}
                         className="block px-2.5 pb-2 pt-2.5 py-2.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
-                        placeholder="  "
+                        placeholder={data.patient_first_name}
                         required
-                        value={data.patient_first_name}
                       />
                       <label
                         htmlFor="firstname"
                         className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-teal-500 peer-focus:dark:text-teal-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-                      >
-                        First Name
-                      </label>
+                      ></label>
                     </div>
                     {/* last Name  */}
                     <div className="col-start-2  relative   ">
