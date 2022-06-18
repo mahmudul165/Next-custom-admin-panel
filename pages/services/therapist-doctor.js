98739fa6eec6fec48f9e0445ebb384d6d5fbbe7d
unknown <mahmudulh883@gmail.com>
2022-06-18 23:18:21 +0600

    added

--- pages/services/therapist-doctor.js
+++ pages/services/therapist-doctor.js
@@ -15,144 +15,145 @@ const schema = yup
   .required();
 function TherapistDoctor() {
   return (
-    <form className="  m-auto   mx-4 my-4 w-full max-w-sm">
-      <div className="card mt-5 p-2">
-        <div className="  md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Category
-            </label>
+    <>
+      {/* <form className="  m-auto   mx-4 my-4 w-full max-w-sm">
+        <div className="card mt-5 p-2">
+          <div className="  md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Category
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <select
+                className="form-select appearance-none
+     block
+     w-full
+     px-3
+     py-1.5
+     text-base
+     font-normal
+     text-gray-700
+     bg-white bg-clip-padding bg-no-repeat
+     border border-solid border-gray-300
+     rounded
+     transition
+     ease-in-out
+     m-0
+     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                aria-label="Default select example"
+              >
+                <option selected>Select Category</option>
+                <option value="1">Category 1</option>
+                <option value="2">Category 2</option>
+              </select>
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <select
-              className="form-select appearance-none
-      block
-      w-full
-      px-3
-      py-1.5
-      text-base
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              aria-label="Default select example"
-            >
-              <option selected>Select Category</option>
-              <option value="1">Category 1</option>
-              <option value="2">Category 2</option>
-            </select>
-          </div>
-        </div>
 
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Sub category
-            </label>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Sub category
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <select
+                className="form-select appearance-none
+     block
+     w-full
+     px-3
+     py-1.5
+     text-base
+     font-normal
+     text-gray-700
+     bg-white bg-clip-padding bg-no-repeat
+     border border-solid border-gray-300
+     rounded
+     transition
+     ease-in-out
+     m-0
+     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                aria-label="Default select example"
+              >
+                <option selected>Select Sub Category</option>
+                <option value="1">Sub Category 1</option>
+                <option value="2">Sub Category 2</option>
+              </select>
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <select
-              className="form-select appearance-none
-      block
-      w-full
-      px-3
-      py-1.5
-      text-base
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              aria-label="Default select example"
-            >
-              <option selected>Select Sub Category</option>
-              <option value="1">Sub Category 1</option>
-              <option value="2">Sub Category 2</option>
-            </select>
-          </div>
-        </div>
 
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Therapist Name
-            </label>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Therapist Name
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="text"
+                placeholder="Jane Doe"
+              />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
-              type="text"
-              placeholder="Jane Doe"
-            />
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Therapist Details
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <textarea
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-6 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="text"
+                placeholder="Jane Doe"
+              ></textarea>
+            </div>
           </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Therapist Details
-            </label>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Reamrks
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="text"
+                placeholder="Jane Doe"
+              />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <textarea
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-6 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
-              type="text"
-              placeholder="Jane Doe"
-            ></textarea>
-          </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Reamrks
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
-              type="text"
-              placeholder="Jane Doe"
-            />
-          </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="inline-full-name"
-            >
-              Status
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <select
-              className="form-select appearance-none
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                Status
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <select
+                className="form-select appearance-none
 block
 w-full
 px-3
@@ -167,31 +168,32 @@ transition
 ease-in-out
 m-0
 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              aria-label="Default select example"
-            >
-              <option selected>Select Status</option>
-              <option value="A">Active</option>
-              <option value="I">Inactive</option>
-              <option value="D">D</option>
-              <option value="P">P</option>
-              <option value="C">C</option>
-            </select>
+                aria-label="Default select example"
+              >
+                <option selected>Select Status</option>
+                <option value="A">Active</option>
+                <option value="I">Inactive</option>
+                <option value="D">D</option>
+                <option value="P">P</option>
+                <option value="C">C</option>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="button"
-            >
-              Save
-            </button>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+              >
+                Save
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form> */}
+    </>
   );
 }
 
