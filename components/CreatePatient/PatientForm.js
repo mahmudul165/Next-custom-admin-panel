51e73713ca3d0c37628eb217d4d4ea5caf2809b8
lamptechs <career@lamptechs.com>
2022-06-21 22:36:42 +0600

    added

--- components/CreatePatient/PatientForm.js
+++ components/CreatePatient/PatientForm.js
@@ -220,14 +220,14 @@ function PaitentForm({ title, data }) {
                     <div className="col-start-1 relative  ">
                       <input
                         type="text"
-                        id="country_id"
-                        {...register("country_id")}
+                        id="country"
+                        {...register("country")}
                         className="block px-2.5 pb-2 pt-2.5 py-2.5 w-full rows-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
                         placeholder="  "
                         required
                       />
                       <label
-                        htmlFor="country_id"
+                        htmlFor="country"
                         className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-teal-500 peer-focus:dark:text-teal-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                       >
                         Country
@@ -259,7 +259,7 @@ function PaitentForm({ title, data }) {
                       <input
                         type="text"
                         id="state_city"
-                        {...register("state_id")}
+                        {...register("state")}
                         className="block px-2.5 pb-2 pt-2.5 py-2.5 w-full rows-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
                         placeholder="  "
                         required
@@ -811,14 +811,14 @@ function PaitentForm({ title, data }) {
                     <div className="col-start-1 relative  ">
                       <input
                         type="text"
-                        id="country_id"
-                        {...register("country_id")}
+                        id="country"
+                        {...register("country")}
                         className="block px-2.5 pb-2 pt-2.5 py-2.5 w-full rows-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
                         placeholder="  "
                         required
                       />
                       <label
-                        htmlFor="country_id"
+                        htmlFor="country"
                         className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-teal-500 peer-focus:dark:text-teal-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                       >
                         Country
@@ -850,7 +850,7 @@ function PaitentForm({ title, data }) {
                       <input
                         type="text"
                         id="state_city"
-                        {...register("state_id")}
+                        {...register("state")}
                         className="block px-2.5 pb-2 pt-2.5 py-2.5 w-full rows-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
                         placeholder="  "
                         required
@@ -1018,8 +1018,8 @@ function PaitentForm({ title, data }) {
                     <div className=" relative   ">
                       {" "}
                       <select
-                        id="blood_group_id"
-                        {...register("blood_group_id")}
+                        id="blood_group"
+                        {...register("blood_group")}
                         className="block px-2.5 pb-2 pt-2.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
                       >
                         <option selected>Select blood group</option>
