c52f14b708ca8c48244b55684249a92ceeee1071
lamptechs <career@lamptechs.com>
2022-06-23 20:54:54 +0600

    added

--- pages/account/login.js
+++ pages/account/login.js
@@ -15,16 +15,7 @@ function Login() {
           <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
             Login to your account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or
-            {/* <a
-      href="#"
-      className="font-medium text-indigo-600 hover:text-indigo-500"
-    >
-      {" "}
-      start your 14-day free trial{" "}
-    </a> */}
-          </p>
+          <p className="mt-2 text-center text-sm text-gray-600">Or</p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
           <div className="relative">
